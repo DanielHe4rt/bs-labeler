7e1c0c1afd3705069478fae3bd5e11ebb15e5c5e
danielhe4rt <hey@danielheart.dev>
2024-09-12 15:10:14 -0300

    feat: better logs

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { Bot, Post } from '@skyware/bot';
 import 'dotenv/config';
 import { LabelType } from './type.js';
 import {tags} from './tags.js';
+import chalk from 'chalk';
 
 
 const allTags = Object
@@ -38,32 +39,30 @@ server.db.prepare('SELECT * FROM labels_definitions').all().forEach((row: any) =
 
 
 bot.on('like', async ({ subject, user }) => {
-  console.log(user.handle + `(${user.did})` + ' liked ' + subject.uri);
 
+  const handle = chalk.underline(user.handle);
   if (!(subject instanceof Post)) {
-    console.log(' -> [x] Subject is not a post');
+    console.log(chalk.cyan("[L]" + handle + `(${user.did})` + ' liked the labeler'));
     return;
   }
 
   const label = availableLabels.get(subject.uri);
   if (!label) {
-    console.log(' -> [x] Post isn\'t related to any label');
+    console.log(chalk.magenta("[L]" + handle + `(${user.did})` + ' liked a random post! (thx)'));
     return;
   }
+  
 
   if (label.delete_trigger) {
-    console.log(' -> [v] Clearing ' + user.handle + ' labels');
     let userLabels = server.db.prepare('SELECT * FROM labels WHERE uri = ?').all(user.did);
-    console.log(" -> [v] Negating " + userLabels.map((label: any) => label.val));
+    console.log(chalk.red('[D] Deleting ' + handle + ' labels: ' + userLabels.map((label: any) => label.val)));
 
-    let response = await server.createLabels({ uri: user.did }, { negate: [...allTags, 'clear'] });
-    console.log(" -> [v] Response: " + response);
+    await server.createLabels({ uri: user.did }, { negate: [...allTags, 'clear'] });
     
     server.db.prepare('DELETE FROM labels WHERE uri = ?').run(user.did);
     return;
   }
-
-  console.log(' -> [v] Labeling ' + user.handle + ' with ' + label.name);
   await user.labelAccount([label.slug]);
+  console.log(chalk.green('[N] Labeling ' + handle + ' with ' + label.name + '... (' + user.labels.length + ' labels)'));
 });
 
